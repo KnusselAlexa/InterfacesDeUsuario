b9837a00b6ff91fc551b080dc4d5629e7950e835
KnusselAlexa <115746704+KnusselAlexa@users.noreply.github.com>
2023-10-07 18:20:17 -0300

    avance de footer y pagina de juego

--- TP2/EntregaFinal/js/main.js
+++ TP2/EntregaFinal/js/main.js
@@ -3,13 +3,67 @@ function startPage() {
 
     "use strict"
 
+    let homeLogo = document.querySelector('#home-logo');
+
     let navBtn = document.querySelector('#nav-btn');
+    let navList = document.querySelector('.nav-bar');
+
+    let btnCarrito = document.querySelector('#id-carrito');
+    let menuCarrito = document.querySelector('#menu-nav');
+
+    let btnUsuario = document.querySelector('#id-usuario');
+    let menuUsuario = document.querySelector('#user-nav');
+    let closeSessionBtn = document.querySelector('#close-session');
+
+    let navCategoriasH3 = document.querySelectorAll('.div-categorias h3');
+
+    let btnPlay = document.querySelector("#play-btn-4inline");
+
+    homeLogo.addEventListener('click', navigateHome);
 
     navBtn.addEventListener('click', openNavbar);
 
+    btnCarrito.addEventListener('click', openCartMenu);
+
+    btnUsuario.addEventListener('click', openUserMenu);
+
+    btnPlay.addEventListener('click', goGamePage);
+
+    closeSessionBtn.addEventListener('click', closeSession);
+
+    function navigateHome() {
+        window.location.replace("home.html");
+    }
+
     function openNavbar() {
-        let navList = document.querySelector('.nav-bar');
         navList.classList.toggle("nav-bar-open");
         navBtn.classList.toggle("rotate");
+        setTimeout(() => {
+            navCategoriasH3.forEach((s) => {
+                s.classList.toggle("oculto");
+            });
+        }, 100);
+    }
+
+    function openCartMenu() {
+        menuCarrito.classList.toggle("opacity-transition");
+        menuCarrito.classList.toggle("oculto");
+        menuUsuario.classList.add("opacity-transition");
+        menuUsuario.classList.add("oculto");
+    }
+
+    function openUserMenu() {
+        menuUsuario.classList.toggle("opacity-transition");
+        menuUsuario.classList.toggle("oculto");
+        menuCarrito.classList.add("opacity-transition");
+        menuCarrito.classList.add("oculto");
+    }
+
+    function goGamePage() {
+        window.location.replace("game.html");
+    }
+
+    function closeSession() {
+        window.location.replace("home.html");
     }
 }
